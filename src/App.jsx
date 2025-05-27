9dc5e3e2bbac7c2a99135bf217106a37dda1ff8b
andrew0samir <andrewsamir317@gmail.com>
2025-05-27 21:22:15 +0300

    implementing main pages and routes , linking routes using navlink , using css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+import NotFound from "./pages/NotFound";
+import AppLayout from "./pages/AppLayout";
+
 function App() {
-    return (
-        <div>
-            world wise
-        </div>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
+
+function HomePage() {
+  return (
+    <div>
+      <PageNav />
+      <AppNav />
+      <h1>world wise</h1>
+
+      <Link to="/app">go to app </Link>
+    </div>
+  );
+}
+
+export default HomePage;
