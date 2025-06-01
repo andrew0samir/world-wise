eace6c8c3f50dcd0a803eef46624af366adf8b59
andrew0samir <andrewsamir317@gmail.com>
2025-06-01 21:34:20 +0300

    consuming the cities context

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -36,5 +36,12 @@ function CitiesProvider({ children }) {
   );
 }
 
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined) {
+    throw new Error("Cities context was used outside the CitiesProvider");
+  }
+  return context;
+}
 
-export {CitiesProvider}
+export { CitiesProvider, useCities };
